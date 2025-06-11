a449c286aeb52bc247c2197f39375557325c3825
ALIF-AL-RAZI <alifalrazi1@gmail.com>
2025-06-11 11:09:58 +0600

    https

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://www.alifalrazi.com' || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://www.alifalrazi.com' || 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
